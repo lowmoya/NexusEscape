12d79d1e4249798742f4a82f78144b48fba62b16
lowmoya <lowmoya@gmail.com>
2023-11-14 16:27:48 -0500

    Layer fix for level 1

--- Entities/enemies/swordenemy.gd
+++ Entities/enemies/swordenemy.gd
@@ -10,6 +10,7 @@ extends Entity
 @export var speed = 100
 @export var attack_delay = .5
 
+var n_sprite = null
 var n_held = null
 var n_weapon = null
 var n_player = null
@@ -34,8 +35,7 @@ var current_attack_delay = 0
 
 func _ready():
 	# Load node references
-	#n_held = get_node("Held")
-	#n_weapon = n_held.get_node("Gun")
+	n_sprite = get_node("Sprite")
 	n_player = get_tree().current_scene.get_node("Player")
 	
 	# Initialize state
@@ -78,15 +78,7 @@ func _physics_process(delta):
 	# Adjust velocity and move
 	var move = Vector2(n_player.velocity + n_player.global_position - global_position).normalized()
 	velocity = lerp(velocity, move * (speed + dash_bonus), acceleration * delta)
+	n_sprite.scale.x = -4 if velocity.x < 0 else 4
 	move_and_slide()
+
 	
-	# Adjust held angle for simple predictive aiming
-	#var aim_offset = n_player.global_position - global_position
-	#n_held.rotation = atan(aim_offset.y / aim_offset.x)
-	#n_held.scale.x = -1 if aim_offset.x < 0 else 1
-	
-	# Decrement delay, when it hits zero attack and reset it
-	#current_attack_delay -= delta
-	#if current_attack_delay <= 0:
-	#	n_weapon.attack()
-	#	current_attack_delay = attack_delay
