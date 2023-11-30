8de67a20aa2dcdf2dfcbeb8475f56de1808de32f
val <idkstartingnew@gmail.com>
2023-11-29 22:55:45 -0500

    skeleton tilemap node for lvl4

--- Levels/TileMap.gd
+++ Levels/TileMap.gd
@@ -1,13 +1,9 @@
 extends TileMap
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if name == "level2":
 		flip_horizontally()
 	
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func flip_horizontally():
 	scale.x *= -1
