ab0fba2fd81542e8dacb4bc53ce9fcb7c1e6a685
val <idkstartingnew@gmail.com>
2023-12-01 20:54:45 -0500

    Revert "lvl 4 stuff? revert back if needed if it doesn't work"
    This reverts commit 3fe192ccd539930004773ebf671e05e428491d34.

--- Levels/level4dialoguecontroller.gd
+++ Levels/level4dialoguecontroller.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
+@export var n_dialogue: Control
 
-@export var n_enemycontroller: Node2D
-@export var n_door: StaticBody2D
 
 func dialogue_heard():
 	pass
-	
-func _process(_delta):
-	if n_enemycontroller.enemy_count == 0 and n_enemycontroller.idle_enemy_count == 0:
-		n_door.queue_free()
-		process_mode = Node.PROCESS_MODE_DISABLED
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	n_dialogue.call_deferred("say", "Did I do it? Did I make it out?\n\nNo. There's still [b]something[/b] up ahead.\n\nWhat is that thing?")
+
