20a1844cc14f82288624f4556ac46202c9263dd4
lowmoya <lowmoya@gmail.com>
2023-12-01 20:57:39 -0500

    Retreived menu

--- Levels/Level4DialogueController.gd
+++ Levels/Level4DialogueController.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
-
+@export var n_dialogue: Control
 @export var n_enemycontroller: Node2D
 @export var n_door: StaticBody2D
 
 func dialogue_heard():
 	pass
+
+func _ready():
+	n_dialogue.call_deferred("say", "Did I do it? Did I make it out?\n\nNo. There's still [b]something[/b] up ahead.\n\nWhat is that thing?")
 	
+
 func _process(_delta):
 	if n_enemycontroller.enemy_count == 0 and n_enemycontroller.idle_enemy_count == 0:
 		n_door.queue_free()
